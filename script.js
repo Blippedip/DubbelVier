0ddce2fb3d5398e50e3bf97e8dd61fea8f917d82
Blippedip <jennifergerrits02@gmail.com>
2026-09-16 10:17:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const mainNav = document.querySelector(".main-nav");
+
+if (menuToggle && mainNav) {
+  menuToggle.addEventListener("click", () => {
+    const open = mainNav.classList.toggle("is-open");
+    menuToggle.setAttribute("aria-expanded", String(open));
+  });
+
+  mainNav.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", () => {
+      mainNav.classList.remove("is-open");
+      menuToggle.setAttribute("aria-expanded", "false");
+    });
+  });
+}
+
+document.getElementById("year").textContent = new Date().getFullYear();
